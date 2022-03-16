036010f1588bee9cde37c78421a5864ae0415f0c
ap23hp <aprakhanna5@gmail.com>
2022-03-16 05:38:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import React from "react";
+import "./style.css";
+
+
+import {Box,
+  Toolbar, Typography,Button,IconButton,AppBar,
+  Card,CardActions,CardContent
+,Grid,Accordion,AccordionSummary,AccordionDetails
+
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+
+
+export default function App() {
+  return (
+    <div style={{padding:"2px"}}>
+  <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Guvi
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+<br/>
+<Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          XYZ Organization,
+        </Typography>
+      
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+     of India
+        </Typography>
+        <Typography variant="body2">
+         Learning is the key to success
+          <br />
+         
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+<br/>
+<Grid container spacing={2}>
+
+<Grid item>
+<Button variant="contained">Admin Login</Button>
+</Grid>
+
+<Grid item>
+<Button variant="contained">Student Login</Button>
+</Grid>
+
+</Grid>
+<Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>React js</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Node js</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion disabled>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Angular js</Typography>
+        </AccordionSummary>
+      </Accordion>
+<br/>
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
